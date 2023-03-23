120e36229de9b15e79b428acdf9fe004ecb75cf1
Fender, Patrick <patricktfender+git@gmail.com>
2023-03-23 17:58:15 +0100

    add clerk auth (working)

---
+++ src/middleware.ts
@@ -0,0 +1,17 @@
+/**
+ * because this is file called 'middleware.ts', Next will automatically run this as middleware on every request before they reach the server
+ */
+
+import { withClerkMiddleware } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export default withClerkMiddleware((req: NextRequest) => {
+  console.log("middleware running");
+  return NextResponse.next();
+});
+
+// Stop Middleware running on static files
+export const config = {
+  matcher: "/((?!_next/image|_next/static|favicon.ico).*)",
+};
